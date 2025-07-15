a3a0c4ebbb8edacae47b124ca1983a262af1b66f
Seaseals <seaiayumi27@gmail.com>
2025-07-15 11:17:57 +0900

    kadai_005を再提出する

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -3,8 +3,8 @@ let userWeight;
 let userHeight;
 
 // 値の代入
-userWeight="68";
-userHeight="1.7";
+userWeight=68;
+userHeight=1.7;
 
 // コンソールへの出力
-console.log(userWeight/ (userHeight*userHeight) );
+console.log(68/ (1.7*1.7) );
